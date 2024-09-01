6b11f8ed4339442428c03e51b09f4d1166d9a0bc
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-09-01 16:27:21 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,3 +3,18 @@
 #define SYMM_GRAPHICS_IMPLEMENTATION
 #define SYMM_AUDIO_IMPLEMENTATION
 #include "include/symm.h"
+
+int main() {
+    symm_graphics::InitGraphics();
+    symm_audio::InitAudio();
+  // I will add other stuff here soon...
+
+    // Main loop
+    while (true) {
+        symm_graphics::RenderFrame();
+        symm_audio::PlaySound();
+        // Handle other operations
+    }
+
+    return 0;
+}
