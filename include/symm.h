2a4961f110d37bb906d666bcbbd06de3de85f651
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-09-01 16:25:49 +0500

    Update, add some stuff

--- include/symm.h
+++ include/symm.h
@@ -1,4 +1,32 @@
-#define SYMM
-#define SYMM_IMPLEMENTATION
-#define SYMM_GRAPHICS_IMPLEMENTATION
-#define SYMM_AUDIO_IMPLEMENTATION
+#ifndef SYMM_H
+#define SYMM_H
+
+#ifdef SYMM_IMPLEMENTATION
+#define SYMM_API extern "C"
+#else
+#define SYMM_API
+#endif
+
+// Define the graphics module
+#ifdef SYMM_GRAPHICS_IMPLEMENTATION
+namespace symm_graphics {
+    SYMM_API void InitGraphics();
+    SYMM_API void RenderFrame();
+    SYMM_API void createObject();
+    SYMM_API void renderShaders();
+    SYMM_API void SymmCreateShaders();
+}
+#endif
+
+// Define the audio module
+#ifdef SYMM_AUDIO_IMPLEMENTATION
+namespace symm_audio {
+    SYMM_API void InitAudio();
+    SYMM_API void PlaySound();
+    SYMM_API void replayAudio();
+    SYMM_API void AudioTimestamp();
+}
+#endif
+
+#endif // SYMM_H
+
